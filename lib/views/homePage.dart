ade5055ac1fa48b854a6587f00cd1413ab88f4dd
richard <richardo.aog@gmail.com>
2022-09-15 10:41:05 +0700

    first add

--- lib/views/homePage.dart
+++ lib/views/homePage.dart
@@ -1,25 +1,5 @@
-import 'package:belajar_flutter/screen/list_view_home.dart';
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          appBarTheme: const AppBarTheme(foregroundColor: Colors.black)),
-      home: const MyStatefulWidget(),
-    );
-  }
-}
+import 'package:belajar_flutter/screen/list_view_home.dart';
 
 class MyStatefulWidget extends StatefulWidget {
   const MyStatefulWidget({super.key});
