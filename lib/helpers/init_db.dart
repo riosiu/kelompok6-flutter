56d35252119df81961b2f680968dc50048bd7569
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-09-28 16:32:39 +0700

    fix: fix init db sql wrong syntax

--- lib/helpers/init_db.dart
+++ lib/helpers/init_db.dart
@@ -7,7 +7,7 @@ Future<Database> initDb() async {
 
   Database db = await openDatabase(path, version: 1, onCreate: ((db, version) {
     return db.execute(
-        "CREATE TABLE saved_book(id INTEGER AUTOINCREMENT PRIMARY KEY, google_books_volume_id TEXT, page INTEGER, started_reading TEXT, finished_reading TEXT, UNIQUE(google_books_volume_id))");
+        "CREATE TABLE saved_book(id INTEGER PRIMARY KEY AUTOINCREMENT, google_books_volume_id TEXT, page INTEGER, started_reading TEXT, finished_reading TEXT, UNIQUE(google_books_volume_id))");
   }));
 
   return db;
