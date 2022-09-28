3c99231ed05f30428ca067e68977b03bfd895af2
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-09-28 16:33:07 +0700

    feat: add book card save button

---
+++ lib/widget/book_card_save_button.dart
@@ -0,0 +1,42 @@
+import 'package:booktrackers/classes/book.dart';
+import 'package:booktrackers/screens/saved_screen.dart';
+import 'package:booktrackers/services/saved_book_service.dart';
+import 'package:booktrackers/widget/update_page_dialog.dart';
+import 'package:flutter/material.dart';
+
+class BookCardSaveButton extends StatelessWidget {
+  final Book book;
+  final SavedBook? savedBook;
+
+  const BookCardSaveButton({Key? key, required this.book, this.savedBook})
+      : super(key: key);
+
+  void saveBook() {
+    SavedBook.add(book).then((_) => null);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: ButtonStyle(
+        foregroundColor:
+            MaterialStateProperty.all<Color>(Theme.of(context).primaryColor),
+      ),
+      onPressed: () {
+        if (savedBook?.page == null) {
+          saveBook();
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => const SavedScreen()));
+        } else {
+          showDialog(
+              context: context,
+              builder: ((context) =>
+                  UpdatePageDialog(savedBook: savedBook!, book: book!)));
+        }
+      },
+      child: savedBook?.page != null && book.pageCount != null
+          ? Text("${savedBook!.page * 100 ~/ book.pageCount!}%")
+          : const Icon(Icons.add),
+    );
+  }
+}
