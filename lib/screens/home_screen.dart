f5dbd06f6f464299ede290e4f4fc5e134d11d274
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-10-19 11:08:35 +0700

    feat: add chip for showing current level and skill

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:booktrackers/widget/book_card.dart';
 import 'package:booktrackers/widget/my_bottom_navigation_bar.dart';
 import 'package:booktrackers/helpers/theme.dart';
 
@@ -30,37 +29,39 @@ class HomeScreenState extends State<HomeScreen> {
                 child: Column(children: <Widget>[
                   Center(
                     child: Container(
-                        margin: const EdgeInsets.only(top: 128, bottom: 64),
+                        margin: const EdgeInsets.only(top: 128, bottom: 16),
                         child: const Image(
                           image: AssetImage('assets/images/logotext.png'),
                           height: 80,
                         )),
                   ),
+                  Container(
+                      margin: const EdgeInsets.only(bottom: 48),
+                      child: Chip(
+                        label: Column(children: [
+                          Text("Level 12"),
+                          Text(
+                            "420xp",
+                            style: TextStyle(fontSize: 8),
+                          )
+                        ]),
+                        avatar: CircularProgressIndicator(
+                          strokeWidth: 3.0,
+                          value: 0.41,
+                          color: Theme.of(context).primaryColor,
+                          backgroundColor: Colors.black12,
+                        ),
+                      ))
                 ])),
             Container(
                 padding:
                     const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                child: const Text("Baru saja ditambahkan ke daftar tersimpan",
+                child: const Text("Lanjut membaca",
                     style: TextStyle(
                         fontWeight: FontWeight.w600,
                         fontSize: 14,
                         color: Colors.black54))),
-            // BookCard(
-            //   title: "Nineteen Eighty-four",
-            //   year: 1949,
-            //   coverImageSrc:
-            //       "http://books.google.com/books/content?id=JT_JDAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-            //   description:
-            //       "Sepanjang hidupnya, Winston berusaha menjadi warga negara yang baik dengan mematuhi setiap aturan Partai meski jauh di dalam hati dan pikirannya bersemayam antipati terhadap kediktatoran yang ada di negaranya. Walaupun begitu, Winston tidak berani melakukan perlawanan secara terbuka. Tidak mengherankan, karena Polisi Pikiran, teleskrin, dan mikrofon tersembunyi membuat privasi hanya serupa fantasi. Bahkan, sejarah ditulis ulang sesuai kehendak Partai. Negara berkuasa mutlak atas rakyatnya. ",
-            // ),
-            // const BookCard(
-            //   title: "Nineteen Eighty-four",
-            //   year: 1949,
-            //   coverImageSrc:
-            //       "http://books.google.com/books/content?id=JT_JDAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
-            //   description:
-            //       "Sepanjang hidupnya, Winston berusaha menjadi warga negara yang baik dengan mematuhi setiap aturan Partai meski jauh di dalam hati dan pikirannya bersemayam antipati terhadap kediktatoran yang ada di negaranya. Walaupun begitu, Winston tidak berani melakukan perlawanan secara terbuka. Tidak mengherankan, karena Polisi Pikiran, teleskrin, dan mikrofon tersembunyi membuat privasi hanya serupa fantasi. Bahkan, sejarah ditulis ulang sesuai kehendak Partai. Negara berkuasa mutlak atas rakyatnya. ",
-            // )
+            
           ],
         ),
         bottomNavigationBar: MyBottomNavigationBar(
