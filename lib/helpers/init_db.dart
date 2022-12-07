f58510f5390fb621f5960bd15161232c513825fa
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2022-12-07 16:52:34 +0700

    fix: null check

--- lib/helpers/init_db.dart
+++ lib/helpers/init_db.dart
@@ -5,9 +5,14 @@ Future<Database> initDb() async {
   var databasesPath = await getDatabasesPath();
   String path = join(databasesPath, 'booktracker.db');
 
-  Database db = await openDatabase(path, version: 1, onCreate: ((db, version) {
-    return db.execute(
-        "CREATE TABLE saved_book(id INTEGER PRIMARY KEY AUTOINCREMENT, google_books_volume_id TEXT, page INTEGER, started_reading TEXT, finished_reading TEXT, UNIQUE(google_books_volume_id))");
+  Database db = await openDatabase(path, version: 1, onCreate: ((db, version) async {
+    await db.execute("""
+        CREATE TABLE saved_book(id INTEGER PRIMARY KEY AUTOINCREMENT, google_books_volume_id TEXT, page INTEGER, started_reading TEXT, finished_reading TEXT, UNIQUE(google_books_volume_id));
+        """);
+    await db.execute("""
+        CREATE TABLE book_log(id INTEGER PRIMARY KEY AUTOINCREMENT, google_books_volume_id TEXT, time TEXT, event_type TEXT);
+        """);
+    return;
   }));
 
   return db;
